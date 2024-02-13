21678d948f53f7e9997d18a935dc4a2fad3f5903
Dima <babaha3000@gmail.com>
2024-02-13 21:47:18 +0200

    UAC-14: new update for vercel.json

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,9 +51,9 @@ INSTALLED_APPS = [
 GRAPHENE = {"SCHEMA": "backend.schema.schema"}
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
