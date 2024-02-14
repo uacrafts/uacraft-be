d37005f7741509df15f414d189df6be08a3fdbff
Dima <babaha3000@gmail.com>
2024-02-14 22:05:53 +0200

    UAC-14: new file structure

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,6 +1,16 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from http.server import BaseHTTPRequestHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
