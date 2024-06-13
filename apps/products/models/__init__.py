ada512b3496f7401029d0585a12ffc2f94faee3d
Stanislav <nstanislass@gmail.com>
2024-06-13 16:07:19 +0300

    import Brand, Category, Price, Product, Store

---
+++ apps/products/models/__init__.py
@@ -0,0 +1,5 @@
+from apps.products.models.brand import Brand
+from apps.products.models.category import Category
+from apps.products.models.price import Price
+from apps.products.models.product import Product
+from apps.products.models.store import Store
