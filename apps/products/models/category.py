b625bbd6e8b4cb0a45745449a1b1c632b64888fa
Stanislav <nstanislass@gmail.com>
2024-06-13 16:11:47 +0300

    add Category

---
+++ apps/products/models/category.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from slugify import slugify
+from unidecode import unidecode
+
+from apps.common.models import TimestampMixin
+
+
+class Category(TimestampMixin, models.Model):
+    title = models.CharField(_('Назва категорії'), max_length=100)
+
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children',
+        verbose_name=_('Батьківська категорія')
+    )
+    slug = models.SlugField(max_length=100, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if self.title:
+            self.slug = slugify(unidecode(self.title))
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'product_category'
+        verbose_name = _('Категорію')
+        verbose_name_plural = _('Категорії')
+        ordering = ('title',)
