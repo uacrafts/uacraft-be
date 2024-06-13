5f014c0ebd7540fefb0b1ebc17f9ef3b98391dc2
Stanislav <nstanislass@gmail.com>
2024-06-13 16:17:40 +0300

    add StoreSerializer

---
+++ apps/products/serializers/store.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from apps.products.models import Store
+
+
+class StoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Store
+        fields = (
+            'pk',
+            'title',
+            'city',
+            'is_displayed',
+            'created_at',
+            'updated_at'
+        )
+        read_only_fields = ('created_at', 'updated_at')
