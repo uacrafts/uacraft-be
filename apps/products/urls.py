4307234b3459ef59547c929d0433c723f8657fef
Stanislav <nstanislass@gmail.com>
2024-06-13 16:17:59 +0300

    add router categories, products

---
+++ apps/products/urls.py
@@ -0,0 +1,13 @@
+from rest_framework import routers
+
+from apps.products import views
+
+app_name = 'api_products'
+
+router = routers.SimpleRouter()
+router.register('categories', views.CategoryAPIViewSet, basename='category')
+router.register('products', views.ProductAPIViewSet, basename='product')
+
+urlpatterns = []
+
+urlpatterns += router.urls
