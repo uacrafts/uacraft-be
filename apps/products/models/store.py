5659f8d06391a0555b0348aac297f03be9338ebf
Stanislav <nstanislass@gmail.com>
2024-06-13 16:17:34 +0300

    add Store

---
+++ apps/products/models/store.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from apps.common.models import TimestampMixin
+
+
+class Store(TimestampMixin, models.Model):
+    class Status(models.IntegerChoices):
+        DEACTIVATE = 0, _('Деактивовано')
+        ACTIVE = 1, _('Активовано')
+
+    title = models.CharField(_('Назва магазину'), max_length=255, db_index=True)
+    city = models.CharField(_('Місто'), max_length=255)
+    is_displayed = models.BooleanField(
+        choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+        db_index=True,
+        default=Status.ACTIVE,
+        verbose_name=_('Статус')
+    )
+
+    def __str__(self):
+        return f'{self.title} | {self.city}'
+
+    class Meta:
+        db_table = 'product_store'
+        verbose_name = _('Магазин')
+        verbose_name_plural = _('Магазини')
+        unique_together = ('title',)
