a055ccfa2a854892638afbb24d72f6363645c750
Stanislav <nstanislass@gmail.com>
2024-06-13 16:11:32 +0300

    add BrandSerializer

---
+++ apps/products/serializers/brand.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from apps.products.models import Brand
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = (
+            'pk',
+            'title',
+            'created_at',
+            'updated_at'
+        )
+
+        read_only_fields = ('created_at', 'updated_at')
