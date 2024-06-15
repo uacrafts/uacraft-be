a464353b9d85b30ead8257e4b9714109caf2d6b3
Stanislav <nstanislass@gmail.com>
2024-06-15 03:19:44 +0300

    import ReadOnlyAdminModifyPermission, ACCESS_DENIED_ERROR, UNAUTHORIZED_ERROR

---
+++ apps/common/permissions/__init__.py
@@ -0,0 +1,3 @@
+from apps.common.permissions.read_only_admin_modify_permission import ReadOnlyAdminModifyPermission
+from apps.common.permissions.responses import ACCESS_DENIED_ERROR
+from apps.common.permissions.responses import UNAUTHORIZED_ERROR
