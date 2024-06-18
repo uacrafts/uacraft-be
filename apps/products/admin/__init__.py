783864af33d6b3e8b33d71cef4054be83975c63a
Stanislav <nstanislass@gmail.com>
2024-06-18 11:01:17 +0300

    import ProductImageAdmin

--- apps/products/admin/__init__.py
+++ apps/products/admin/__init__.py
@@ -2,4 +2,5 @@ from apps.products.admin.brand import BrandAdmin
 from apps.products.admin.category import CategoryAdmin
 from apps.products.admin.price import PriceAdmin
 from apps.products.admin.product import ProductAdmin
+from apps.products.admin.product_image import ProductImageAdmin
 from apps.products.admin.store import StoreAdmin
