a058ab44196492cd3bf4a3db1d17231eddb4e239
Stanislav <nstanislass@gmail.com>
2024-06-18 20:05:53 +0300

    rename store -> seller

--- apps/products/models/__init__.py
+++ apps/products/models/__init__.py
@@ -3,4 +3,4 @@ from apps.products.models.category import Category
 from apps.products.models.price import Price
 from apps.products.models.product import Product
 from apps.products.models.product_image import ProductImage
-from apps.products.models.store import Store
+from apps.products.models.seller import Seller
