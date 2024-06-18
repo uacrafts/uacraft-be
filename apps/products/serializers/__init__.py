884b7d1a7ed02fa8aa94df63536a00e4e9d111a4
Stanislav <nstanislass@gmail.com>
2024-06-18 20:06:00 +0300

    rename store -> seller

--- apps/products/serializers/__init__.py
+++ apps/products/serializers/__init__.py
@@ -2,4 +2,4 @@ from apps.products.serializers.brand import BrandSerializer
 from apps.products.serializers.category import CategorySerializer
 from apps.products.serializers.price import PriceSerializer
 from apps.products.serializers.product import ProductSerializer
-from apps.products.serializers.store import StoreSerializer
+from apps.products.serializers.seller import SellerSerializer
