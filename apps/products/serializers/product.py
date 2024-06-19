56e4db297d4c82ad10b9ddc184cf98848269de6d
Stanislav <nstanislass@gmail.com>
2024-06-19 14:25:44 +0300

    change name method

--- apps/products/serializers/product.py
+++ apps/products/serializers/product.py
@@ -1,22 +1,23 @@
-from typing import Type
+from typing import List
 
+from django.utils.translation import gettext_lazy as _
+from drf_spectacular.utils import extend_schema
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from apps.products.models import Product
+from apps.products.models import ProductImage
 from apps.products.serializers.brand import BrandSerializer
 from apps.products.serializers.category import CategorySerializer
 from apps.products.serializers.price import PriceSerializer
-from apps.products.serializers.product_image import ProductImageSerializer
-from apps.products.serializers.store import StoreSerializer
+from apps.products.serializers.seller import SellerSerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    store = serializers.SerializerMethodField()
+    seller = serializers.SerializerMethodField()
     price = serializers.SerializerMethodField()
     brand = serializers.SerializerMethodField()
     category = serializers.SerializerMethodField()
-    images = ProductImageSerializer(many=True, read_only=True)
     image_urls = serializers.SerializerMethodField()
 
     class Meta:
@@ -24,29 +25,22 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = (
             'id',
             'title',
-            'store',
+            'seller',
             'price',
             'brand',
             'category',
-            'images',
             'image_urls',
             'description',
-            'is_stock',
+            'quantity_in_stock',
             'created_at',
             'updated_at'
         )
-        read_only_fields = ('created_at', 'updated_at', 'image_url')
+        read_only_fields = ('created_at', 'updated_at', 'image_urls')
 
-    @extend_schema_field(StoreSerializer)
-    def get_store(self, product: Product) -> dict[str, any] | None:
-        if product.store:
-            return StoreSerializer(product.store).data
-        return None
-
-    @extend_schema_field(PriceSerializer)
-    def get_price(self, product: Product) -> dict[str, any] | None:
-        if product.price:
-            return PriceSerializer(product.price).data
+    @extend_schema_field(SellerSerializer)
+    def get_seller(self, product: Product) -> dict[str, any] | None:
+        if product.seller:
+            return SellerSerializer(product.seller).data
         return None
 
     @extend_schema_field(BrandSerializer)
@@ -55,19 +49,22 @@ class ProductSerializer(serializers.ModelSerializer):
             return BrandSerializer(product.brand).data
         return None
 
+    @extend_schema_field(PriceSerializer)
+    def get_price(self, product: Product) -> dict[str, any] | None:
+        if product.price:
+            return PriceSerializer(product.price).data
+        return None
+
     @extend_schema_field(CategorySerializer)
     def get_category(self, product: Product) -> dict[str, any] | None:
         if product.category:
             return CategorySerializer(product.category).data
         return None
 
-    @extend_schema_field(Type[list[str]])
-    def get_image_urls(self, product: Product) -> list[str]:
+    @extend_schema(
+        description=_('List of image URLs for the product.'),
+        responses=serializers.ListField(child=serializers.CharField())
+    )
+    def get_image_urls(self, product: Product) -> List[str]:
         request = self.context.get('request')
-        if not request:
-            return []
-
-        image_urls = []
-        for image in product.images.all():
-            image_urls.append(request.build_absolute_uri(image.image.url))
-        return image_urls
+        return [request.build_absolute_uri(image.image.url) for image in product.product_images.all()]
