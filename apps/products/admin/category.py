996e2e82cc67d429a4d33fb0bff67d4bea80b82c
Stanislav <nstanislass@gmail.com>
2024-06-22 13:45:40 +0300

    add list_display, fields, readonly_fields

--- apps/products/admin/category.py
+++ apps/products/admin/category.py
@@ -5,4 +5,7 @@ from apps.products.models import Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'parent', 'slug', 'created_at', 'updated_at')
+    fields = ('title', 'parent', 'slug', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
     prepopulated_fields = {'slug': ('title',)}
