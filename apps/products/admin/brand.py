500813898b50a8c1791b600a8206c7e5d23e12a6
Stanislav <nstanislass@gmail.com>
2024-06-22 13:45:28 +0300

    add list_display, fields, readonly_fields

--- apps/products/admin/brand.py
+++ apps/products/admin/brand.py
@@ -5,4 +5,6 @@ from apps.products.models import Brand
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('title', 'created_at', 'updated_at')
+    fields = ('title', 'logo', 'description', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
