ef286e4a2c969258481f1855a9c3ec2131effc2c
Stanislav <nstanislass@gmail.com>
2024-06-22 13:44:23 +0300

    add list_display, list_display_links, raw_id_fields

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -6,4 +6,6 @@ User = get_user_model()
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('email', 'is_email_verified', 'is_seller', 'is_staff', 'is_active', 'date_joined')
+    list_display_links = ('email',)
+    raw_id_fields = ('user_permissions',)
